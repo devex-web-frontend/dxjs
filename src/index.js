8e187d6b52d98a2faec7fdc45f24035344c79d88
Artem Parhomenko <mankdev@users.noreply.github.com>
2015-08-11 15:47:33 +0300

    fix pathes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import dxCore from 'dx.core';
-import dxBem from 'dx.bem';
-import dxDate from 'dx.date';
-import dxDom from 'dx.dom';
-import dxEvent from 'dx.event';
-import dxMeasure from 'dx.measure';
-import dxString from 'dx.string';
-import dxTmpl from 'dx.tmpl';
+import dxCore from './dx.core';
+import dxBem from './dx.bem';
+import dxDate from './dx.date';
+import dxDom from './dx.dom';
+import dxEvent from './dx.event';
+import dxMeasure from './dx.measure';
+import dxString from './dx.string';
+import dxTmpl from './dx.tmpl';
 
 export default {
 	dxCore,
